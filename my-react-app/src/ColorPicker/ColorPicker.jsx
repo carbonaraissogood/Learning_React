3e1176ccdb7c1c138121c0da167577f7ddef03a1
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-06-11 16:16:36 +0800

    Format ColorPicker component for improved readability

--- my-react-app/src/ColorPicker/ColorPicker.jsx
+++ my-react-app/src/ColorPicker/ColorPicker.jsx
@@ -10,14 +10,19 @@ function ColorPicker() {
 
   return(
     <div className={styles.colorPickerContainer}>
+
       <h1>Color Picker</h1>
+
       <div
         className={styles.colorDisplay}
         style={{backgroundColor: color}}>
           <p>Selected Color: {color}</p>
       </div>
+
       <label>Select a Color:</label>
+
       <input type="color" value={color} onChange={handleColorChange} />
+      
     </div>
   );
 }
