c962e3014646d8b0873ce5ea62c6918bd3ab456b
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-06-13 14:58:36 +0800

    Fix state update in Counter component to use functional updates for increment and decrement

--- my-react-app/src/Counter/Counter.jsx
+++ my-react-app/src/Counter/Counter.jsx
@@ -5,11 +5,11 @@ function Counter() {
   const [count, setCount] = useState(0);
 
   const increment = () => {
-    setCount(count + 1);
+    setCount(prevCount => count + 1);
   }
 
   const decrement = () => {
-    setCount(count - 1);
+    setCount(prevCount => count - 1);
   }
 
   const reset = () => {
