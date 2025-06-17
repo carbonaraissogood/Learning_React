43f79f992803e76abcd244a4c87693f3ecb2afad
carbonaraissogood <fernandezmaxxinne14@gmail.com>
2025-06-17 11:42:31 +0800

    Add various components and styles for user interaction and data management
    - Created OnChange.module.css for styling the OnChange component.
    - Implemented List component with List.module.css for displaying items.
    - Developed MyComponent for state management with user interactions.
    - Added MyComponent1 and MyComponent2 for car and food management respectively.
    - Introduced ProfilePicture component with associated styles.
    - Created Student component to display user information.
    - Developed ToDoApp with functionality for adding, editing, and deleting tasks.
    - Added UserForm and UserFormV2 for user data input with validation.
    - Implemented UserGreeting component for conditional rendering based on login status.
    - Created button component for user interaction.
    - Established main entry point for the application.
    - Added global styles in index.css for consistent styling across components.

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,8 @@ import ToDoApp from './ToDoApp/ToDoApp.jsx'
 // import MyComponent from './MyComponent1/MyComponent';
 // import MyComponent from './MyComponent2/MyComponent';
 // import MyComponent from './Cars_Ver2/MyComponent';
-import UserForm from './UserForm/UserForm';
-import UserFormV2 from './UserForm_Ver2/UserFormV2';
+import UserForm from './UserForm/UserForm.jsx';
+import UserFormV2 from './UserForm_Ver2/UserFormV2.jsx';
 
 function App() {
 
